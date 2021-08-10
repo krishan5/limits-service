048e026608c6b999ec690c9e9cca182a300d7d17
Krishan Kumar <kktech970@gmail.com>
2021-08-10 23:27:31 +0530

    Hard coded and application.properties level configuration

---
+++ src/main/java/com/kk/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,26 @@
+package com.kk.microservices.limitsservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kk.microservices.limitsservice.configuration.Configuration;
+import com.kk.microservices.limitsservice.entity.Limits;
+
+@RestController
+public class LimitsController {
+	
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public Limits retriveLimits() {
+		return new Limits(1, 1000);
+	}
+	
+	@GetMapping("/limits-using-configuration")
+	public Limits retriveLimitsUsingConfiguration() {
+		return new Limits(configuration.getMinimum(), configuration.getMaximum());
+	}
+	
+}
